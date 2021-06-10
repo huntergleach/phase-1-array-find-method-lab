24e0d56e622b5767c7723a3d429236a81ebf769d
huntergleach <hunter.leach21@gmail.com>
2021-06-10 12:13:19 -0500

    commit tree 9319c43745d7a65ce2ce0311fbcc6b283957bccd

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(array) {
+    const results = array.find(record => record.result === "W") 
+    return !!results ? results.year : undefined 
+    // if (results === true) {
+//         return results.year
+//     } else {
+//         return undefined
+//     }
+}
